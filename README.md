553f26f47d7fce1e0bdaf02d9ac94d5694c6d129
PiStacks <ruan.ru.bekker@gmail.com>
2020-05-30 01:25:25 +0200

    add vim configs

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # dotfiles
 RaspberryPi Stacks Dotfiles
+
+## Vim
+
+Features:
+
+- https://github.com/junegunn/vim-plug
+- https://github.com/itchyny/lightline.vim
+- https://github.com/tomasr/molokai
--- setup.sh
+++ setup.sh
@@ -3,4 +3,11 @@
 # setup vim
 mkdir -p ~/.vim/colors
 curl -o ~/.vim/colors/molokai.vim https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
+
+# install vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# copy .vimrc config
 cp .vimrc ~/.vimrc
+
+echo "run vim and execute :PlugInstall"
