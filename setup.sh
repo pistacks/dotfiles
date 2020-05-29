e06e22659dddc7b801a30ee772bbeccc13afd032
PiStacks <ruan.ru.bekker@gmail.com>
2020-05-30 00:29:40 +0200

    add vim

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# setup vim
+mkdir -p ~/.vim/colors
+curl -o ~/.vim/colors/molokai.vim https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim
+cp .vimrc ~/.vimrc
